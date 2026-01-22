65cf74a52b734eceda3731cfe54c9317ee08bf3e
Dave Bevan <dave.bevan@evobytes.co.uk>
2026-01-22 08:49:00 +0000

    5.0.3 inc manifest

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='netbox_component_synchronization',
-    version='5.0.2',
+    version='5.0.3',
     description='Syncing existing components with the components from a device type template in NetBox 4+',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Keith Knowles and Bastian Leicht and Dave Bevan',
     author_email='mkknowles@outlook.com',
     license='GPL-3.0',
-    packages=["netbox_component_synchronization"],
-    package_data={"netbox_component_synchronization": ["templates/netbox_component_synchronization/*.html"]},
+    packages=find_packages(),
+    include_package_data=True,
     zip_safe=False
 )
