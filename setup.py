13cbbf20d941835de7aa268fa515aefbc24d377b
Dave Bevan <dave.bevan@evobytes.co.uk>
2026-01-22 09:03:54 +0000

    Fix packaging, again

--- setup.py
+++ setup.py
@@ -5,14 +5,19 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='netbox_component_synchronization',
-    version='5.0.3',
+    version='5.0.4',
     description='Syncing existing components with the components from a device type template in NetBox 4+',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Keith Knowles and Bastian Leicht and Dave Bevan',
     author_email='mkknowles@outlook.com',
     license='GPL-3.0',
-    packages=find_packages(),
-    include_package_data=True,
+    packages=["netbox_component_synchronization"],
+    package_data={
+        "netbox_component_synchronization": [
+            "templates/netbox_component_synchronization/*.html",
+            "migrations/*.py",
+        ]
+    },
     zip_safe=False
 )
