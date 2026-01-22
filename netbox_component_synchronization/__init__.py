0bfbc7f033a0c451d142fea4cb386337e23d04f5
Dave Bevan <dave.bevan@evobytes.co.uk>
2026-01-22 17:50:19 +0000

    bump version

--- netbox_component_synchronization/__init__.py
+++ netbox_component_synchronization/__init__.py
@@ -5,7 +5,7 @@ class Config(PluginConfig):
     name = "netbox_component_synchronization"
     verbose_name = "Component Synchronization Plugin"
     description = "Easily synchronize device components with their device types through an accessible UI."
-    version = "5.0.5"
+    version = "5.0.6"
     author = "Keith Knowles and Bastian Leicht and Dave Bevan"
     author_email = "mkknowles@outlook.com"
     website = "https://github.com/evobytes/netbox-component-synchronization/"
