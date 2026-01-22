66ef4e69b16d1501e3fd90d271cdca4b305870fb
Dave Bevan <dave.bevan@evobytes.co.uk>
2026-01-22 17:39:50 +0000

    bump version, add website links

--- netbox_component_synchronization/__init__.py
+++ netbox_component_synchronization/__init__.py
@@ -5,9 +5,10 @@ class Config(PluginConfig):
     name = "netbox_component_synchronization"
     verbose_name = "Component Synchronization Plugin"
     description = "Easily synchronize device components with their device types through an accessible UI."
-    version = "5.0.4"
+    version = "5.0.5"
     author = "Keith Knowles and Bastian Leicht and Dave Bevan"
     author_email = "mkknowles@outlook.com"
+    website = "https://github.com/evobytes/netbox-component-synchronization/"
     default_settings = {
         "include_interfaces_panel": False,
         # Compare description during diff
--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='netbox_component_synchronization',
-    version='5.0.4',
+    version='5.0.5',
     description='Syncing existing components with the components from a device type template in NetBox 4+',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Keith Knowles and Bastian Leicht and Dave Bevan',
     author_email='mkknowles@outlook.com',
+    url='https://github.com/evobytes/netbox-component-synchronization/',
     license='GPL-3.0',
     packages=["netbox_component_synchronization"],
     package_data={
@@ -18,6 +19,10 @@ setup(
             "templates/netbox_component_synchronization/*.html",
             "migrations/*.py",
         ]
+    },
+    project_urls={
+        "Bug Tracker": "https://github.com/evobytes/netbox-component-synchronization/issues",
+        "Source Code": "https://github.com/evobytes/netbox-component-synchronization/",
     },
     zip_safe=False
 )
