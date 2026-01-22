d0c07a23954a3f6182534ae60bc575d97834e9fb
Dave Bevan <dave.bevan@evobytes.co.uk>
2026-01-22 18:10:22 +0000

    fix migration script, again.

--- netbox_component_synchronization/migrations/0001_initial.py
+++ netbox_component_synchronization/migrations/0001_initial.py
@@ -1,36 +1,30 @@
 from django.db import migrations, models
+import taggit.managers
+import utilities.json
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
-        # NetBox core dependency (safe baseline)
-        ("dcim", "0001_initial"),
+        ('extras', '0001_initial'),
+        ('dcim', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
-            name="ComponentSyncPermission",
+            name='ComponentSyncPermission',
             fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
-                ("created", models.DateTimeField(auto_now_add=True, null=True)),
-                ("last_updated", models.DateTimeField(auto_now=True, null=True)),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', models.DateTimeField(auto_now_add=True, null=True)),
+                ('last_updated', models.DateTimeField(auto_now=True, null=True)),
+                ('description', models.CharField(blank=True, max_length=200)),
+                ('custom_field_data', models.JSONField(blank=True, default=dict, encoder=utilities.json.CustomFieldJSONEncoder)),
+                ('tags', taggit.managers.TaggableManager(through='extras.TaggedItem', to='extras.Tag')),
             ],
             options={
-                "verbose_name": "Synchronization Button",
-                "verbose_name_plural": "Synchronization Button",
-                "permissions": [
-                    ("can_use", "Can use component sync tools"),
-                ],
+                'verbose_name': 'Synchronization Button',
+                'verbose_name_plural': 'Synchronization Button',
+                'permissions': [('can_use', 'Can use component sync tools')],
             },
         ),
     ]
