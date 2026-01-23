36b205cd66de47a7ef55c2eabdb5c9c72843b29d
Dave Bevan <dave.bevan@evobytes.co.uk>
2026-01-23 14:23:01 +0000

    fix: handle table-exists. bump version.

--- netbox_component_synchronization/__init__.py
+++ netbox_component_synchronization/__init__.py
@@ -5,7 +5,7 @@ class Config(PluginConfig):
     name = "netbox_component_synchronization"
     verbose_name = "Component Synchronization Plugin"
     description = "Easily synchronize device components with their device types through an accessible UI."
-    version = "5.0.9"
+    version = "5.0.10"
     author = "Keith Knowles and Bastian Leicht and Dave Bevan"
     author_email = "mkknowles@outlook.com"
     website = "https://github.com/evobytes/netbox-component-synchronization/"
